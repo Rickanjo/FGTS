fb723b586b37cb8d5f0b38547c7cde33ffe9ba54
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-08 13:03:13 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Calcule a parcela do FGTS sobre o salário de um funcionário (8%).
+
+double salario;
+double fgts;
+
+Console.WriteLine("--- Calculo do FGTS ---\n");
+
+Console.Write("Digite o salário do funcionário: ");
+salario = Convert.ToDouble(Console.ReadLine()!);
+
+fgts = salario * 0.08;
+
+Console.WriteLine($"\nParcela do FGTS: R$ {fgts:F2}");
